2d731dee46d6eb38a7fcd614257a6b9dc453d224
000itsuki000 <itsuki.h@collabo-style.co.jp>
2023-10-17 14:43:54 +0900

    Initial Commit

---
+++ testsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testsDemo
+//
+//  Created by Itsuki on 2023/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
